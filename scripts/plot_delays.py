b72b925e015207ff1ab9c80db316086e3bcfe7cf
Max SCHMELLER <max.schmeller@tier4.jp>
2024-04-04 10:26:28 +0900

    Update plotting script to include more percentiles

--- scripts/plot_delays.py
+++ scripts/plot_delays.py
@@ -12,9 +12,10 @@ args = parser.parse_args()
 
 with open(args.log_file, 'r') as f:
   lines = f.readlines()
-  lines = [re.search("#([A-Za-z]{3}) ([0-9]+\.[0-9]+)ms", l) for l in lines]
+  lines = [re.search(r"#([A-Za-z]{3})\s+([0-9]+\.[0-9]+)ms", l) for l in lines]
   lines = [l for l in lines if l]
   lines = [(m[1], float(m[2])) for m in lines]
   df = pd.DataFrame(lines, columns=["tag", 'dt'])
 
-print(df.groupby("tag").describe())
+print(df.groupby("tag").describe([.5, .6, .7, .8, .9, .95, .99, .999, 1 - 1e-5]))
+ 

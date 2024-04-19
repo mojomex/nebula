030986b82330b38346cc11f002fb6975f2f7fb0e
Max SCHMELLER <max.schmeller@tier4.jp>
2024-04-19 14:31:26 +0900

    feat(topic_delay): receive both pointclouds and scan messages

--- topic_delay/include/topic_delay/topic_delay.hpp
+++ topic_delay/include/topic_delay/topic_delay.hpp
@@ -4,6 +4,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include "pandar_msgs/msg/pandar_scan.hpp"
 #include <iostream>
 
 #include <chrono>
@@ -15,11 +16,14 @@ namespace ros
 class TopicDelay final : public rclcpp::Node
 {
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+  rclcpp::Subscription<pandar_msgs::msg::PandarScan>::SharedPtr scan_sub_;
 
 public:
   TopicDelay(const rclcpp::NodeOptions & options);
 
-  void ReceiveScanMsgCallback(const sensor_msgs::msg::PointCloud2::SharedPtr scan_msg);
+  void ReceiveCloudMsgCallback(const sensor_msgs::msg::PointCloud2::UniquePtr cloud_msg);
+
+  void ReceiveScanMsgCallback(const pandar_msgs::msg::PandarScan::UniquePtr scan_msg);
 };
 
 }  // namespace ros

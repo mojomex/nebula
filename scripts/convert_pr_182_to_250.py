397ac073abe87061fc14e60ca1b8a5b819bb0cfe
Max SCHMELLER <max.schmeller@tier4.jp>
2025-02-28 15:10:12 +0900

    chore: update conversion script to support all Hesai sensor models
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- scripts/convert_pr_182_to_250.py
+++ scripts/convert_pr_182_to_250.py
@@ -7,6 +7,18 @@ from PIL import Image
 import numpy as np
 from ruamel.yaml import YAML
 
+MODEL_RESOLUTIONS = {
+    "Pandar40P": (1800, 40),
+    "Pandar64": (1800, 64),
+    "PandarXT16": (2000, 16),
+    "PandarXT32": (2000, 32),
+    "PandarXT32M": (2000, 32),
+    "PandarAT128": (1200, 128),
+    "PandarQT64": (600, 64),
+    "PandarQT128": (900, 128),
+    "Pandar128E4X": (3600, 128),
+}
+
 
 def load_yaml(yaml_path):
     with open(yaml_path, "r") as file:
@@ -22,20 +34,26 @@ def load_yaml(yaml_path):
         sys.exit(1)
 
 
-def generate_mask(ring_section_filter):
-    width, height = 3600, 128
+def generate_mask(ring_section_filter, sensor_model):
+    width, height = MODEL_RESOLUTIONS[sensor_model]
+
+    # Start with a white image (all points enabled)
     mask = np.full((height, width), 255, dtype=np.uint8)
 
+    def azimuth_to_pixel(azimuth):
+        return int(width * azimuth / 360)
+
     for ring, azi_start, azi_end in ring_section_filter:
-        if ring < 0 or ring >= 128:
+        if ring < 0 or ring >= height:
             raise ValueError(f"Invalid ring: {ring}")
 
         if azi_end < azi_start:
-            mask[ring, int(10 * azi_start) : 3600] = 0  # Set pixels to black
-            mask[ring, 0 : int(10 * azi_end)] = 0  # Set pixels to black
-
+            mask[ring, azimuth_to_pixel(azi_start) : width] = 0  # Set pixels to black
+            mask[ring, 0 : azimuth_to_pixel(azi_end)] = 0  # Set pixels to black
         else:
-            mask[ring, int(10 * azi_start) : int(10 * azi_end)] = 0  # Set pixels to black
+            mask[ring, azimuth_to_pixel(azi_start) : azimuth_to_pixel(azi_end)] = (
+                0  # Set pixels to black
+            )
 
     return mask
 
@@ -46,15 +64,22 @@ def save_png(mask, output_path):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <input_yaml>")
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <sensor_model> <input_yaml>")
         sys.exit(1)
 
-    yaml_path = sys.argv[1]
+    sensor_model = sys.argv[1]
+
+    if sensor_model not in MODEL_RESOLUTIONS:
+        print(f"Invalid sensor model: {sensor_model}")
+        print(f"Valid models are: {', '.join(MODEL_RESOLUTIONS.keys())}")
+        sys.exit(1)
+
+    yaml_path = sys.argv[2]
     output_path = yaml_path.rsplit(".", 1)[0] + ".png"
 
     ring_section_filter = load_yaml(yaml_path)
-    mask = generate_mask(ring_section_filter)
+    mask = generate_mask(ring_section_filter, sensor_model)
     save_png(mask, output_path)
 
     print(f"Mask saved to {output_path}")
